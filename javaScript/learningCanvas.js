bdf6ef74ac88f7a212e7749c80a3c097434435bc
Muetzilla <54369574+Muetzilla@users.noreply.github.com>
2021-04-27 14:12:33 +0200

    circles

--- javaScript/learningCanvas.js
+++ javaScript/learningCanvas.js
@@ -66,3 +66,29 @@ function draw() {
       twoTringales.stroke();
     }
   }
+  function drawCircles() {
+    var canvasCircles = document.getElementById('circles');
+    if (canvasCircles.getContext) {
+      var circle = canvasCircles.getContext('2d');
+  
+      for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 3; j++) {
+          circle.beginPath();
+          var x = 25 + j * 50; // x coordinate
+          var y = 25 + i * 50; // y coordinate
+          var radius = 20; // Arc radius
+          var startAngle = 0; // Starting point on circle
+          var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
+          var counterclockwise = i % 2 !== 0; // clockwise or counterclockwise
+  
+          circle.arc(x, y, radius, startAngle, endAngle, counterclockwise);
+  
+          if (i > 1) {
+            circle.fill();
+          } else {
+            circle.stroke();
+          }
+        }
+      }
+    }
+  }
