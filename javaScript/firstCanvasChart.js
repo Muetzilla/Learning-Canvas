ac98a72f35a1618f69766e1157355bf7a181bf9f
Muetzilla <54369574+Muetzilla@users.noreply.github.com>
2021-04-28 14:57:28 +0200

    My first barchart

--- javaScript/firstCanvasChart.js
+++ javaScript/firstCanvasChart.js
@@ -1,47 +1,117 @@
-
-function drawChart(){
-var data = [
-    { y: '2014', a: 50, b: 90},
-    { y: '2015', a: 65,  b: 75},
-    { y: '2016', a: 50,  b: 50},
-    { y: '2017', a: 75,  b: 60},
-    { y: '2018', a: 80,  b: 65},
-    { y: '2019', a: 90,  b: 70},
-    { y: '2020', a: 100, b: 75},
-    { y: '2021', a: 115, b: 75},
-    { y: '2022', a: 120, b: 85},
-    { y: '2023', a: 145, b: 85},
-    { y: '2024', a: 160, b: 95}
-  ],
-  config = {
-    data: data,
-    xkey: 'y',
-    ykeys: ['a', 'b'],
-    labels: ['Total Income', 'Total Outcome'],
-    fillOpacity: 0.6,
-    hideHover: 'auto',
-    behaveLikeLine: true,
-    resize: true,
-    pointFillColors:['#ffffff'],
-    pointStrokeColors: ['black'],
-    lineColors:['gray','red']
-};
-config.element = 'area-chart';
-Morris.Area(config);
-config.element = 'line-chart';
-Morris.Line(config);
-config.element = 'bar-chart';
-Morris.Bar(config);
-/*config.element = 'stacked';
-config.stacked = true;
-Morris.Bar(config);
-Morris.Donut({
-element: 'pie-chart',
-data: [
-  {label: "Friends", value: 30},
-  {label: "Allies", value: 15},
-  {label: "Enemies", value: 45},
-  {label: "Neutral", value: 10}
-]
-});*/
+var myCanvas = document.getElementById("barChart");
+myCanvas.width = 300;
+myCanvas.height = 300;
+  
+var ctx = myCanvas.getContext("2d");
+function drawLine(ctx, startX, startY, endX, endY,color){
+  ctx.save();
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(startX,startY);
+  ctx.lineTo(endX,endY);
+  ctx.stroke();
+  ctx.restore();
 }
+function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height,color){
+  ctx.save();
+  ctx.fillStyle=color;
+  ctx.fillRect(upperLeftCornerX,upperLeftCornerY,width,height);
+  ctx.restore();
+}
+var myVinyls = {
+  "Classical music": 10,
+  "Alternative rock": 14,
+  "Pop": 2,
+  "Jazz": 12
+};
+var Barchart = function(options){
+  this.options = options;
+  this.canvas = options.canvas;
+  this.ctx = this.canvas.getContext("2d");
+  this.colors = options.colors;
+
+  this.draw = function(){
+      var maxValue = 0;
+      for (var categ in this.options.data){
+          maxValue = Math.max(maxValue,this.options.data[categ]);
+      }
+      var canvasActualHeight = this.canvas.height - this.options.padding * 2;
+      var canvasActualWidth = this.canvas.width - this.options.padding * 2;
+
+      //drawing the grid lines
+      var gridValue = 0;
+      while (gridValue <= maxValue){
+          var gridY = canvasActualHeight * (1 - gridValue/maxValue) + this.options.padding;
+          drawLine(
+              this.ctx,
+              0,
+              gridY,
+              this.canvas.width,
+              gridY,
+              this.options.gridColor
+          );
+           
+          //writing grid markers
+          this.ctx.save();
+          this.ctx.fillStyle = this.options.gridColor;
+          this.ctx.font = "bold 10px Arial";
+          this.ctx.fillText(gridValue, 10,gridY - 2);
+          this.ctx.restore();
+
+          gridValue+=this.options.gridScale;
+      }
+
+      //drawing the bars
+      var barIndex = 0;
+      var numberOfBars = Object.keys(this.options.data).length;
+      var barSize = (canvasActualWidth)/numberOfBars;
+
+      for (categ in this.options.data){
+          var val = this.options.data[categ];
+          var barHeight = Math.round( canvasActualHeight * val/maxValue) ;
+          drawBar(
+              this.ctx,
+              this.options.padding + barIndex * barSize,
+              this.canvas.height - barHeight - this.options.padding,
+              barSize,
+              barHeight,
+              this.colors[barIndex%this.colors.length]
+          );
+
+          barIndex++;
+      }
+      //drawing series name
+      this.ctx.save();
+      this.ctx.textBaseline="bottom";
+      this.ctx.textAlign="center";
+      this.ctx.fillStyle = "#000000";
+      this.ctx.font = "bold 14px Arial";
+      this.ctx.fillText(this.options.seriesName, this.canvas.width/2,this.canvas.height);
+      this.ctx.restore();   
+
+  }
+}
+/*ar myBarchart = new Barchart(
+  {
+      canvas:myCanvas,
+      padding:10,
+      gridScale:5,
+      gridColor:"#eeeeee",
+      data:myVinyls,
+      colors:["#a55ca5","#67b6c7", "#bccd7a","#eb9743"]
+  }
+);
+myBarchart.draw();*/
+var myBarchart = new Barchart(
+  {
+      canvas:myCanvas,
+      seriesName:"Vinyl records",
+      padding:20,
+      gridScale:5,
+      gridColor:"#eeeeee",
+      data:myVinyls,
+      colors:["#a55ca5","#67b6c7", "#bccd7a","#eb9743"]
+  }
+);
+myBarchart.draw();
+  
